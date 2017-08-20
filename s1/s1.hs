b3f57c8464d59b55d20637fab8cb10190da09253
Andrew Lei <andrew.w.lei@gmail.com>
2017-08-19 19:13:51 -0700

    modify xor

--- s1/s1.hs
+++ s1/s1.hs
@@ -11,10 +11,7 @@ import qualified Data.Bits as Bits (xor)
 import qualified Data.ByteString as BS (pack)
 import qualified Data.ByteString.Lazy as DBL (zipWith)
 
-import Data.ByteString.Base16 (encode)
 import Data.ByteString.Lazy (cycle, fromStrict)
-
-import Data.Aviary.Birds (cardinal')
 
 --c3
 import Data.ByteString (mapAccumL, singleton)
@@ -31,8 +28,14 @@ import Data.Function (on)
 import Data.ByteString.Char8 (unpack)
 
 --c6
+import Data.ByteString.Base16 (encode)
 import Data.Bits (popCount)
 import Data.Char (digitToInt)
+
+import qualified Data.ByteString.Base64 as B64 (decode)
+import qualified Data.ByteString as BS (take, drop)
+
+import Control.Monad (liftM2)
 
 --c1
 fromHex :: String -> ByteString
@@ -43,15 +46,15 @@ infixl 8 .:
 (.:) = (.) . (.)
 
 infixl 7 .-
-(.-) = cardinal'
+(.-) = (flip .) . (.)
 
 xor :: ByteString -> ByteString -> ByteString
-xor = encode . BS.pack .: DBL.zipWith Bits.xor
+xor = BS.pack .: DBL.zipWith Bits.xor
   .- cycle . fromStrict
   .- fromStrict
 {-
 xor :: ByteString -> ByteString -> ByteString
-xor s1 s2 = encode . BS.pack $ DBL.zipWith Bits.xor s1' s2'
+xor s1 s2 = BS.pack $ DBL.zipWith Bits.xor s1' s2'
   where
     s1' = cycle . fromStrict $ s1
     s2' = fromStrict s2
@@ -80,7 +83,7 @@ w8scorer :: Word8 -> Int
 w8scorer = scorer . chr . fromIntegral
 
 xor1 :: Int -> ByteString -> ByteString
-xor1 = fst . decode .: xor . singleton . fromIntegral
+xor1 = xor . singleton . fromIntegral
 
 xor256 :: ByteString -> [ByteString]
 xor256 = flip map [0..255] . flip xor1
@@ -102,14 +105,53 @@ highscore = snd . head . scoreAndSort
 
 --c5
 repKeyXOR :: ByteString -> ByteString -> ByteString
-repKeyXOR = fst . decode .: xor
+repKeyXOR = xor
 
 repKeyXOR' :: String -> String -> String
 repKeyXOR' = unpack .: repKeyXOR .- pack .- pack
 
 --c6
-hammingdist :: ByteString -> ByteString -> Int
-hammingdist = sum . map (popCount . digitToInt) . unpack .: xor
+hammingdist :: Integral a => ByteString -> ByteString -> a
+hammingdist = sum
+  . map (fromIntegral . popCount . digitToInt)
+  . unpack
+  . encode
+  .: xor
 
-hammingdist' :: String -> String -> Int
+hammingdist' :: Integral a => String -> String -> a
 hammingdist' = hammingdist .- pack .- pack
+
+b64str2bs :: String -> Either String ByteString
+b64str2bs = B64.decode . pack
+
+
+sample :: Integral a => Int -> a -> ByteString -> [ByteString]
+sample size nsamples = helper nsamples []
+  where
+    helper :: Integral a => a -> [ByteString] -> ByteString -> [ByteString]
+    helper 0          acc text = acc
+    helper remSamples acc text = helper (remSamples - 1)
+                                        (BS.take size text : acc)
+                                        (BS.drop size text)
+
+infixl 8 .::
+(.::) = (.) . (.:)
+-- (.) . (.) . (.)
+
+xorblocks :: Integral a => Int -> a -> ByteString -> [ByteString]
+xorblocks =  map (uncurry xor) . (\l -> [ (a, b) | a <- l, b <- l, a < b ]) .:: sample
+
+blockHD :: (Integral a, Floating b) => Int -> a -> ByteString -> b
+blockHD size = mean . map normHD . getPairs .: (sample size)
+  where
+    size' :: Floating b => b
+    size' = fromIntegral size
+
+    normHD :: Floating b => (ByteString, ByteString) -> b
+    normHD = (/size') . fromIntegral . uncurry hammingdist
+
+    getPairs :: Ord c => [c] -> [(c,c)]
+    getPairs list = [ (a, b) | a <- list, b <- list, a < b ]
+
+    mean :: Floating b => [b] -> b
+    mean = liftM2 (/) sum (fromIntegral . length)
--- s1/s1c2.hs
+++ s1/s1c2.hs
@@ -1,8 +1,9 @@
 import S1 (fromHex, xor)
 
 import Data.ByteString.Char8 (unpack)
+import Data.ByteString.Base16 (encode)
 
 main = do
   x <- fromHex <$> readFile "2a.txt"
   y <- fromHex <$> readFile "2b.txt"
-  putStrLn . unpack $ x `xor` y
+  putStrLn . unpack . encode $ x `xor` y
